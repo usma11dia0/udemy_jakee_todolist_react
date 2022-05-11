112835da7607f34348ecf6028dcec8114be265c9
usma11dia0 <usma11dia0@gmail.com>
2022-05-11 18:21:46 +0900

    complete todolist_react

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,13 @@ export const App = () => {
         todoText = {todoText} 
         onChange = {onChangeTodoText} 
         onClick = {onClickAdd}
+        disabled = {incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+      <p style = {{color: "red" }}>
+        登録できるtodoは5個までです。未完了タスクを消化してください。
+        </p>
+      )}
       <IncompleteTodos 
         todos = {incompleteTodos}
         onClickComplete = {onClickComplete}
